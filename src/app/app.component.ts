8c41c5cdfcad58c0b4ece92e37a711983a5b9565
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-09-25 23:02:12 -0300

    Aula2. Incio cap4, PainelComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'caelumpic';
+  
+  title = 'Caelumpic';
+  listaFotos;
+
+  constructor(conexaoApi: HttpClient){
+    
+    conexaoApi.get('http://localhost:3000/v1/fotos')
+              .subscribe(
+                fotosApi => this.listaFotos = fotosApi
+                ,erro => console.log(erro)
+              )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { HttpClientModule } from '@angular/common/http';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
